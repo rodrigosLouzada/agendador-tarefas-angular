77b689af566ea21b3090526caebf958b268013aa
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-10-31 16:33:23 -0300

    old way w/ ng-deep and protection

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
@@ -10,6 +10,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class Register {
 
